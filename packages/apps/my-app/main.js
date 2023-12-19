950227ff7c591de952f17858a7f3c2c61ea879a7
JasonMan34 <JasonErlMan@gmail.com>
2023-12-19 10:46:31 +0200

    try to use dynamic import

--- packages/apps/my-app/main.js
+++ packages/apps/my-app/main.js
@@ -1,8 +1,8 @@
-const { divideBy } = require('@zwi/divide-by');
+import('@zwi/divide-by').then(({ divideBy }) => {
+  exports.main = () => {
+    const a = 2;
+    const b = 5;
 
-exports.main = () => {
-  const a = 2;
-  const b = 5;
-
-  console.log(`${a} / ${b} is ${divideBy(a, b)}`);
-};
+    console.log(`${a} / ${b} is ${divideBy(a, b)}`);
+  };
+});

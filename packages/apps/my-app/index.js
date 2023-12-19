535675ee8f09dad13da4016c4fdff1803c5fc808
JasonMan34 <JasonErlMan@gmail.com>
2023-12-19 15:54:42 +0200

    mjs

--- packages/apps/my-app/index.js
+++
@@ -1,3 +0,0 @@
-import { divideBy } from './divide-by';
-
-console.log(`2 / 5 = ${divideBy(2, 5)}`);
---
+++ packages/apps/my-app/index.mjs
@@ -0,0 +1,3 @@
+import { divideBy } from './divide-by.mjs';
+
+console.log(`2 / 5 = ${divideBy(2, 5)}`);
